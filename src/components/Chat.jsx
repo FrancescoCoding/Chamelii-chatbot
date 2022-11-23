f15c6d9932a1a0ad8cc3a6c3da4d3310e87f00f6
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2022-11-23 18:25:45 +0000

    Fixed some timeouts issues and added some logic

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,3 @@
-import styles from "./Chat.module.css";
-import React, { useState } from "react";
-import { WindupChildren } from "windups";
-
 import Angry from "../assets/Designs/Chameleon_Angry.png";
 import Confused from "../assets/Designs/Chameleon_Confused.png";
 import Excited from "../assets/Designs/Chameleon_Excited.png";
@@ -12,26 +8,76 @@ import SadOpenMouth from "../assets/Designs/Chameleon_Sad (Open).png";
 import Sad from "../assets/Designs/Chameleon_Sad.png";
 import Shrug from "../assets/Designs/Chameleon_Shrug.png";
 
+import styles from "./Chat.module.css";
+import React, { useState, useEffect } from "react";
+import { WindupChildren, Pause } from "windups";
+
 import Ouch from "../assets/Sounds/ouch.mp3";
 
 const Chat = () => {
+  const determineTimeOfDay = () => {
+    const date = new Date();
+    const hour = date.getHours();
+    if (hour >= 0 && hour < 12) {
+      return "morning";
+    } else if (hour >= 12 && hour < 17) {
+      return "afternoon";
+    } else {
+      return "evening";
+    }
+  };
+
   const [emotion, setEmotion] = useState(Neutral);
+  const [isLoading, setIsLoading] = useState(true);
   const [message, setMessage] = useState(
-    <WindupChildren>How was your day?</WindupChildren>
+    <WindupChildren>
+      Good {determineTimeOfDay()}! How was your day?
+    </WindupChildren>
   );
+
+  useEffect(() => {
+    let timer;
+
+    const images = [
+      Angry,
+      Confused,
+      Excited,
+      Grin,
+      Neutral,
+      Pressure,
+      SadOpenMouth,
+      Sad,
+      Shrug,
+    ];
+    images.forEach(image => {
+      new Image().src = image;
+    });
+
+    if (emotion === SadOpenMouth) {
+      timer = setTimeout(() => {
+        setEmotion(Sad);
+      }, 2000);
+    } else if (emotion === Sad) {
+      timer = setTimeout(() => {
+        setEmotion(Neutral);
+      }, 5000);
+    } else {
+      timer = setTimeout(() => {
+        setEmotion(Neutral);
+      }, 4000);
+    }
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [emotion]);
 
   const ouch = new Audio(Ouch);
   const emotionsArray = [Angry, Shrug, Confused, Grin];
-
-  const buttonHandler = () => {
-    console.log("Button clicked");
-  };
-
-  const resetEmotion = () => {
-    setTimeout(() => {
-      setEmotion(Neutral);
-    }, 3000);
-  };
 
   const emotionHandler = () => {
     setEmotion(emotionsArray[Math.floor(Math.random() * emotionsArray.length)]);
@@ -50,9 +96,7 @@ const Chat = () => {
   const mehHandler = () => {
     setEmotion(Confused);
 
-    setMessage(
-      <WindupChildren>{"Why, whats up?"}</WindupChildren>
-    );
+    setMessage(<WindupChildren>{"Why, what's up?"}</WindupChildren>);
   };
 
   const sadHandler = () => {
@@ -61,33 +105,53 @@ const Chat = () => {
     setMessage(
       <WindupChildren>{"Aw, you wanna talk about it?"}</WindupChildren>
     );
-  };
 
+    setTimeout(() => {
+      setMessage(<WindupChildren>{"What's wrong?"}</WindupChildren>);
+    }, 2000);
+  };
 
   return (
     <div className={styles.wrapper}>
-      <h2 style={{ marginBottom: "10px" }}>Hi, I'm a chamelii.</h2>
+      {isLoading && (
+        <>
+          <img src={Grin} alt="Chameleon" className={styles.loader} />
+          <WindupChildren>
+            <h4>Welcome to the Chamelii Chatbot Prototype...</h4>
+            <Pause ms={800} />
+            <h1>
+              Developed and designed by the{" "}
+              <span className={styles.karma}>Karma</span> team!
+            </h1>
+          </WindupChildren>
+        </>
+      )}
+      {!isLoading && (
+        <>
+          <h2 style={{ marginBottom: "10px" }}>Hi, I'm a chamelii.</h2>
 
-      <div className={styles.border}>
-        <img
-          src={emotion}
-          alt={`${emotion}`}
-          onMouseDown={emotionHandler}
-          onMouseUp={() => setEmotion(Neutral)}
-        />
-      </div>
-      <div className={styles.text}>
-        <h2>{message}</h2>
-        <button className={styles.btn} onClick={happyHandler}>
-          My day was great!
-        </button>
-        <button className={styles.btn} onClick={mehHandler}>
-          My day was fine.
-        </button>
-        <button className={styles.btn} onClick={sadHandler}>
-          I had a really bad day.
-        </button>
-      </div>
+          <div className={styles.border}>
+            <img
+              src={emotion}
+              alt={`${emotion}`}
+              onMouseDown={emotionHandler}
+              onMouseUp={() => setEmotion(Neutral)}
+            />
+          </div>
+          <div className={styles.text}>
+            <h2 style={{ height: "60px" }}>{message}</h2>
+            <button className={styles.btn} onClick={happyHandler}>
+              My day was great!
+            </button>
+            <button className={styles.btn} onClick={mehHandler}>
+              My day was fine.
+            </button>
+            <button className={styles.btn} onClick={sadHandler}>
+              I had a really bad day.
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
